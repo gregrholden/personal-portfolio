fe0d7208e9c3570baf13a8891dfc5a95413aeca7
Greg <gregrholden@gmail.com>
2017-08-14 16:43:44 -0400

    removed autoprefixer, used postcss-cssnext in its place. Added README.md file. Ran build again to finalize app

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
@@ -12,7 +11,7 @@ mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/src/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssVars, nested, hexrgba, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, hexrgba, cssNext]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
